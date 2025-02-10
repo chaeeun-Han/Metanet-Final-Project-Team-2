b553e782fbfa733a08ea93bea7fedaf39b8c9cf3
chaeeun-Han <chaeeun.h911@gmail.com>
2025-02-10 13:52:22 +0900

    chore: Modify project groupId and artifactId (#62)

--- MetanetProject/src/main/java/com/example/myapp/certification/service/ICertificationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.certification.service;
-
-public interface ICertificationService {
-
-	boolean checkCourable(Long memberUID, String lecture_id);
-
-	byte[] getCertification(Long memberUID, String lecture_id);
-
-}
--- MetanetProject/src/main/java/com/example/myapp/member/model/Email.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.member.model;
-
-import lombok.Getter;
-
-@Getter
-public class Email {
-	private String email;
-	private String verifyCode;
-}
