b66d35cefad6190db19ae66dd2a723edcaca7271
udongmandu <qjawns10000@naver.com>
2025-02-16 22:06:52 +0900

    hotfix: Add CrossOrigin

--- MetanetProject/src/main/java/com/classpick/web/member/controller/AuthRestController.java
+++ MetanetProject/src/main/java/com/classpick/web/member/controller/AuthRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "https://bamjun.click", allowCredentials = "true")
 public class AuthRestController {
 
 	@Autowired
